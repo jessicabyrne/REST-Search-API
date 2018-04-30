d1b4424f75d4926c10aef13b6ec1a6e9f8098dc8
Byrne, Jessica <Jessica.Byrne@cdk.com>
2018-04-29 18:45:35 -0700

    added timeout for rate limit, matched hardcoded keyword in description

--- app/controllers/ProductController.js
+++ app/controllers/ProductController.js
@@ -6,7 +6,7 @@
 const url = 'http://api.walmartlabs.com/v1/items/';
 const suffix = '?format=json&apiKey=kjybrqfdgp3u4yv2qzcnjndj';
 const http = require('http');
-const Promise = require('request-promise');
+const requestPromise = require('request-promise');
 const productids = [
   14225185,
   14225186,
@@ -29,14 +29,34 @@ const productids = [
 ];
 
 const getProduct = async productid => {
-  return Promise(url + productid + suffix);
+  return requestPromise(url + productid + suffix);
 };
 
-exports.getProducts = async () => {
-  let request;
-  for (let i = 0; i <= productids.length; i++) {
-    console.log(productids[i]);
-    request = await getProduct(productids[i]);
-    console.log(request);
+//write a simple function to delay call
+const delay = (t, val) => {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve(val);
+    }, t);
+  });
+};
+
+exports.getProducts = async (req, res) => {
+  const keyword = 'backpack';
+  let productObject;
+  let productInformation = [];
+  let selectedProducts = [];
+
+  for (let id of productids) {
+    productObject = await getProduct(id);
+    await delay(100);
+    productObject = JSON.parse(productObject); //convert string to JSON object
+    shortDescription = productObject['shortDescription']; //get the short description
+    productId = productObject['itemId'];
+    if (shortDescription.search(keyword) > 0) {
+      selectedProducts = selectedProducts.concat(productId);
+    }
+    productInformation.push(productObject);
   }
+  res.send(selectedProducts);
 };
